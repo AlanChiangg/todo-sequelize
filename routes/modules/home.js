f61e2f3fa915e11d2ace9dbf3a802833a44ad193
AlanChiangg <alaln35alan@gmail.com>
2023-07-26 17:22:40 +0800

    add home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
 
 router.get('/', (req, res) => {
-  res.render('home')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then(todos => res.render('home', { todos: todos }))
+    .catch(error => res.status(422).json(error))
 })
 
 module.exports = router
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../../models')
-const Todo = db.Todo
 const User = db.User
 
 router.get('/login', (req, res) => {
