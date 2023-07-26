507c5dc5175f3b4b98f08c6f39ba680d4ae5df36
AlanChiangg <alaln35alan@gmail.com>
2023-07-26 18:09:18 +0800

    add message partials

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+const flash = require('connect-flash')
 const routes = require('./routes')
 const app = express()
 const PORT = 3000
@@ -13,6 +13,8 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(flash())
+
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
@@ -24,6 +26,8 @@ usePassport(app)
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
